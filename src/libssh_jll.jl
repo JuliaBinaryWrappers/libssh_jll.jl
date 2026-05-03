05fff5b5273c5db82ede02f2bdcb2bf45bec600d
jlbuild <juliabuildbot@gmail.com>
2026-05-03 21:12:31 +0000

    libssh_jll build 0.11.4+0

--- src/libssh_jll.jl
+++ src/libssh_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("libssh")
-JLLWrappers.@generate_main_file("libssh", UUID("a8d4f100-aa25-5708-be18-96e0805c2c9d"))
+JLLWrappers.@generate_main_file("libssh", Base.UUID("a8d4f100-aa25-5708-be18-96e0805c2c9d"))
 end  # module libssh_jll
--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -16,7 +16,7 @@ function __init__()
 
     JLLWrappers.@init_library_product(
         libssh,
-        "lib/libssh.4.10.3.dylib",
+        "lib/libssh.4.10.4.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
