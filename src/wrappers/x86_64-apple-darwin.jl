8ea44e6849491d06b80b8ab488289055620a55f9
jlbuild <juliabuildbot@gmail.com>
2026-08-01 16:41:18 +0000

    libssh_jll build 0.12.2+0

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -16,7 +16,7 @@ function __init__()
 
     JLLWrappers.@init_library_product(
         libssh,
-        "lib/libssh.4.11.1.dylib",
+        "lib/libssh.4.12.0.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
